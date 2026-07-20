13c79924c3b5d1af392cb9b3c3d89ae4e0c21e01
Alejandro <armentaayalaalejandro@gmail.com>
2026-07-19 22:05:25 -0600

    alejandro

---
+++ src/components/CreatePost.jsx
@@ -0,0 +1,24 @@
+export function CreatePost() {
+  return (
+    <form
+      onSubmit={(event) => {
+        event.preventDefault()
+      }}
+    >
+      <div>
+        <label htmlFor='create-title'>Title: </label>
+        <input type='text' id='create-title' name='create-title' />
+      </div>
+      <br />
+      <div>
+        <label htmlFor='create-author'>Author: </label>
+        <input type='text' id='create-author' name='create-author' />
+      </div>
+      <br />
+      <textarea />
+      <br />
+      <br />
+      <input type='submit' value='Create' />
+    </form>
+  )
+}
