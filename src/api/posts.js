98ad011bd05873efb2cabff01926af0402309000
Alejandro <armentaayalaalejandro@gmail.com>
2026-07-20 18:58:39 -0600

    finished creating post

--- src/api/posts.js
+++ src/api/posts.js
@@ -1,3 +1,13 @@
+export const createPost = async (post) => {
+  const createdPost = await fetch(`${import.meta.env.VITE_BACKEND_URL}/posts`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(post),
+  })
+
+  return await createPost.json()
+}
+
 export const getPosts = async (queryParams) => {
   //here only the headers!
   const value = await fetch(
--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -1,24 +1,66 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { createPost } from '../api/posts'
+import { useState } from 'react'
+
 export function CreatePost() {
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [contents, setContents] = useState('')
+
+  //esto solo crea la funcion para llamarse despues no ahorita
+  const queryClient = useQueryClient()
+  const createPostMutation = useMutation({
+    mutationFn: () => createPost({ title, author, contents }),
+    onSuccess: queryClient.invalidateQueries(['posts']),
+  })
+
+  const onSubmit = (event) => {
+    event.preventDefault()
+    //this executes the mutation
+    createPostMutation.mutate()
+  }
+
   return (
-    <form
-      onSubmit={(event) => {
-        event.preventDefault()
-      }}
-    >
+    <form onSubmit={onSubmit}>
       <div>
         <label htmlFor='create-title'>Title: </label>
-        <input type='text' id='create-title' name='create-title' />
+        <input
+          type='text'
+          id='create-title'
+          name='create-title'
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
       </div>
       <br />
       <div>
         <label htmlFor='create-author'>Author: </label>
-        <input type='text' id='create-author' name='create-author' />
+        <input
+          type='text'
+          id='create-author'
+          name='create-author'
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        />
       </div>
       <br />
-      <textarea />
+      <textarea
+        value={contents}
+        onChange={(e) => setContents(e.target.value)}
+      />
       <br />
       <br />
-      <input type='submit' value='Create' />
+      <input
+        type='submit'
+        value={createPostMutation.isPending ? 'Creating...' : 'Create'}
+        disabled={!title || createPostMutation.isPending}
+      />
+      {createPostMutation.isSuccess ? (
+        <>
+          <br />
+          Post created succesfully!
+        </>
+      ) : null}
     </form>
   )
 }
